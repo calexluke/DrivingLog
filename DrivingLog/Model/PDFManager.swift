729a201376bffae3e61994ce17fb388a4f79a1e4
calexluke <c.alexluke@gmail.com>
2022-02-10 11:49:35 -0600

    2.2.1 share text file to user

--- DrivingLog/Model/PDFManager.swift
+++ DrivingLog/Model/PDFManager.swift
@@ -9,65 +9,75 @@ import SwiftUI
 import Foundation
 
 class PDFManager {
-
-  //  a blank Indiana Driving Log would go here
-  init() {
-
-  }
-
-  //Looking for the .documentDirectory, so we can save the pdf there
-  func documentDirectory() -> String {
-    let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory,
-                                                                .userDomainMask,
-                                                                true)
-    return documentDirectory[0]
-  }
-
-  // Adding the path component to the file name (which the pdf will be under)
-  private func append(to pathToDocsDir: String, with fileName: String) -> String? {
-    if var pathURL = URL(string: pathToDocsDir) {
-      pathURL.appendPathComponent(fileName)
-
-      return pathURL.absoluteString
+    
+    //  a blank Indiana Driving Log would go here
+    init() {
+        
     }
-    return nil
-  }
-
-  //Reading in our file, in this case, it would be the Indiana Driving Log
-  func readFileFromDocuments(fileName: String) {
-      guard let filePath = self.append(to: self.documentDirectory(),
-                                       with: fileName) else {
-                                        return
+    
+    //Looking for the .documentDirectory, so we can save the pdf there
+    func documentDirectory() -> String {
+        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                                                    .userDomainMask,
+                                                                    true)
+        return documentDirectory[0]
     }
-      
-    do {
-        let savedString = try String(contentsOfFile: filePath)
-        print(savedString)
-    } catch {
-        print("Error reading saved file")
+    
+    // Adding the path component to the file name (which the pdf will be under)
+    private func append(to pathToDocsDir: String, with fileName: String) -> String? {
+        if var pathURL = URL(string: pathToDocsDir) {
+            pathURL.appendPathComponent(fileName)
+            
+            return pathURL.absoluteString
+        }
+        return nil
     }
-  }
-
-  // Saving the pdf to the documents directory
-  func save(text: String,
-                  toDirectory directory: String,
-                  withFileName fileName: String) {
-      
-      guard let filePath = self.append(to: directory,
-                                       with: fileName) else {
-        return
+    
+    //Reading in our file, in this case, it would be the Indiana Driving Log
+    func readFileFromDocuments(fileName: String) {
+        guard let filePath = self.append(to: self.documentDirectory(),
+                                         with: fileName) else {
+            return
+        }
+        
+        do {
+            let savedString = try String(contentsOfFile: filePath)
+            print(savedString)
+        } catch {
+            print("Error reading saved file")
+        }
     }
-    do {
-        try text.write(toFile: filePath,
-                       atomically: true,
-                       encoding: .utf8)
-    } catch {
-        print("Error", error)
-        return
+    
+    func getFileURLFromDocsDir(fileName: String) -> URL? {
+        guard let filePath = self.append(to: self.documentDirectory(),
+                                         with: fileName) else {
+            return nil
+        }
+        
+        let url = URL(fileURLWithPath: filePath)
+        return url
     }
-    print("Save successful")
-  }
-
-
-
+    
+    // Saving the pdf to the documents directory
+    func save(text: String,
+              toDirectory directory: String,
+              withFileName fileName: String) {
+        
+        guard let filePath = self.append(to: directory,
+                                         with: fileName) else {
+            return
+        }
+        do {
+            try text.write(toFile: filePath,
+                           atomically: true,
+                           encoding: .utf8)
+        } catch {
+            print("Error", error)
+            return
+        }
+        print("Save successful")
+    }
+    
+    
+    
 }
--- DrivingLog/Views/ProgressView.swift
+++ DrivingLog/Views/ProgressView.swift
@@ -33,12 +33,33 @@ struct ProgressView: View {
             }
             .modifier(ButtonModifier())
             .padding()
+            
+            Button("Share document") {
+                shareMockDrivingData()
+            }
+            .modifier(ButtonModifier())
         }
         .sheet(isPresented: $newTripSheetIsPresented, content: {
             TripInProgressView(drivingLog: drivingLog)
         })
         .navigationTitle("Overall Progress")
     }
+    
+    func shareMockDrivingData() {
+        let manager = PDFManager()
+        let docsDir = manager.documentDirectory()
+        let message = "Driving Data will go here"
+        let filename = "DrivingData.txt"
+        manager.save(text: message, toDirectory: docsDir, withFileName: filename)
+        if let documentURL = manager.getFileURLFromDocsDir(fileName: filename) {
+            actionSheet(itemToShare: documentURL)
+        }
+    }
+    
+    func actionSheet(itemToShare: URL) {
+            let activityVC = UIActivityViewController(activityItems: [itemToShare], applicationActivities: nil)
+            UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
+        }
 }
 
 struct ProgressView_Previews: PreviewProvider {
