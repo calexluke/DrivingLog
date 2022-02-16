a0936d7d854aeff09e8e9f0d6a8c2ed1632b8721
thqmos <90867589+thqmos@users.noreply.github.com>
2022-02-16 16:38:34 -0600

    Add files via upload

--- DrivingLog/Model/PDFManager.swift
+++ DrivingLog/Model/PDFManager.swift
@@ -4,17 +4,16 @@
 //
 // Created by Thomas Hohnholt on 2/8/22
 //
-
 import SwiftUI
 import Foundation
 
 class PDFManager {
-    
+
     //  a blank Indiana Driving Log would go here
     init() {
-        
+
     }
-    
+
     //Looking for the .documentDirectory, so we can save the pdf there
     func documentDirectory() -> String {
         let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory,
@@ -22,42 +21,45 @@ class PDFManager {
                                                                     true)
         return documentDirectory[0]
     }
-    
+
     // Adding the path component to the file name (which the pdf will be under)
     private func append(to pathToDocsDir: String, with fileName: String) -> String? {
         if var pathURL = URL(string: pathToDocsDir) {
             pathURL.appendPathComponent(fileName)
-            
+
             return pathURL.absoluteString
         }
         return nil
     }
-    
+
     //Reading in our file, in this case, it would be the Indiana Driving Log
     func readFileFromDocuments(fileName: String) {
         guard let filePath = self.append(to: self.documentDirectory(),
                                          with: fileName) else {
             return
         }
-        
+
+        //This function is only necessary for text files, trying to convert to pdf here
+        /*:
         do {
             let savedString = try String(contentsOfFile: filePath)
             print(savedString)
         } catch {
             print("Error reading saved file")
         }
+        */
     }
-    
+
     func getFileURLFromDocsDir(fileName: String) -> URL? {
         guard let filePath = self.append(to: self.documentDirectory(),
                                          with: fileName) else {
             return nil
         }
-        
+
         let url = URL(fileURLWithPath: filePath)
         return url
     }
-    
+
     func getFilePathFromDocsDir(fileName: String) -> String? {
         var path: String?
         let docsDir = documentDirectory()
@@ -66,16 +68,17 @@ class PDFManager {
         }
         return path
     }
-    
+
     // Saving the pdf to the documents directory
     func save(text: String,
               toDirectory directory: String,
               withFileName fileName: String) {
-        
         guard let filePath = self.append(to: directory,
                                          with: fileName) else {
             return
         }
+        //Writing stuff to text files deleted, unnecessary, but can be added back
+        /*:
         do {
             try text.write(toFile: filePath,
                            atomically: true,
@@ -84,20 +87,21 @@ class PDFManager {
             print("Error", error)
             return
         }
+        */
         print("Save successful")
     }
-    
-    
-    
+
+
+
     func clearFileInDocsDir(_ fileName: String) {
         overWriteFileInDocsDir(fileName, with: "")
     }
-    
+
     func overWriteFileInDocsDir(_ fileName: String, with text: String) {
         guard let filePath = getFilePathFromDocsDir(fileName: fileName) else {
             return
         }
-        
+        /*:
         do {
             try text.write(toFile: filePath,
                            atomically: true,
@@ -106,15 +110,16 @@ class PDFManager {
             print("Error writing to document", error)
             return
         }
+        */
         print("Save successful")
     }
-    
-    func appendToFileInDocsDir(_ fileName: String, with text: String) {        
+
+    func appendToFileInDocsDir(_ fileName: String, with text: String) {
         guard let fileURL = getFileURLFromDocsDir(fileName: fileName) else {
             print("Error getting file path URL")
             return
         }
-        
+
         if let handle = try? FileHandle(forWritingTo: fileURL) {
             handle.seekToEndOfFile() // moving pointer to the end
             handle.write(text.data(using: .utf8)!) // adding content
@@ -123,9 +128,44 @@ class PDFManager {
         } else {
             print("Error appending to file")
         }
-        
+
     }
-    
-    
-    
+
+
+
 }
+
+
+//THIS IS TEST STUFF, NEEDS TO BE INTEGRATED!!
+//Writing to the pdf
+import UIKit
+import PDFKit
+
+//PDF URL needs to be modified a hair, this is just some code
+let pdfURL = ...
+let document = PDFDocument(url: pdfURL!)
+let page1 = document!.page(at: 0)
+
+let field = page1?.annotations.filter({ $0.fieldName! == "Date" })
+//Start time needs to be in mm/dd/yyyy format
+field?[0].widgetStringValue = trip.startTime
+
+//Once when check box is implemented, run an if statement. This is just a skeleton for what is to come
+//We also need to do some calculations for minutes, this once again is just a start
+var night = true
+if night == true{
+  let field = page1?.annotations.filter({ $0.fieldName! == "Night" })
+  //Start time needs to be in mm/dd/yyyy format
+  //Test values
+  field?[0].widgetStringValue = "1h55m"
+}
+else{
+  let field = page1?.annotations.filter({ $0.fieldName! == "Day" })
+  //Start time needs to be in mm/dd/yyyy format
+  //Test values
+  field?[0].widgetStringValue = "45m"
+}
+
+//saveURL also needs to be modified a hair, this is just some code
+let saveURL = ...
+document!.write(to: saveURL)
