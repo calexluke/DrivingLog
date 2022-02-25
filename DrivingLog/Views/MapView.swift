95901fb0902edb6bac8a167ec2848cbc7d22e5c0
thqmos <90867589+thqmos@users.noreply.github.com>
2022-02-24 19:51:59 -0600

    Feature 3.1 - Added user location to the map

--- DrivingLog/Views/MapView.swift
+++ DrivingLog/Views/MapView.swift
@@ -6,20 +6,20 @@
 //
 import SwiftUI
 import MapKit
+import CoreLocation
 
 
 struct MapView: View {
 
-    @State private var region = MKCoordinateRegion(
-        //This will be replaced with GPS data, but for now, hardcoded Valparaiso values
-        center: CLLocationCoordinate2D(latitude: 41.4731, longitude: 87.0611),
-
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-
-    )
+    @StateObject private var viewModel = MapViewModel()
 
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+          .ignoresSafeArea()
+          .accentColor(Color(.systemPink))
+          .onAppear {
+            viewModel.checkIfLocationServicesIsEnabled()
+          }
     }
 
 }
@@ -29,4 +29,49 @@ struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
     }
+}
+
+//Location authorization stuff
+//When integrated, stuff in xcode is going to have to happen
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate{
+  var locationManager: CLLocationManager?
+  @Published private var region = MKCoordinateRegion(
+      //This will be replaced with GPS data, but for now, hardcoded Valparaiso values
+      center: CLLocationCoordinate2D(latitude: 41.4731, longitude: 87.0611),
+      span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+
+  )
+
+  func checkIfLocationServicesIsEnabled() {
+    if CLLocationManager.locationServicesEnabled(){
+      locationManager = CLLocationManager();
+      //Something with activity type maybe?
+      locationManager!.delegate = self
+    }
+    else{
+      print("Print an alert saying that location is not on")
+    }
+  }
+
+  private func checkLocationAuthorization() {
+    guard let locationManager = locationManager else { return }
+
+    swicth locationManager.authorizationStatus{
+      case .notDetermined:
+        locationManager.requestWhenInUseAuthorization()
+      case .restricted:
+        print("Your location is restricted.")
+      case .denied:
+        print("Your have denied location services, go to settings to change it.")
+      case. authorizedAlways:
+        break
+      case .authorizedWhenInUse:
+        region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MKCoordinateSpan)
+      @unknown default
+        break
+    }
+  }
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager){
+    checkLocationAuthorization()
+  }
 }
