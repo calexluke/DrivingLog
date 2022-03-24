f02df7bf1ca49ce8c3368dacec5171176dd9bb7a
thqmos <90867589+thqmos@users.noreply.github.com>
2022-03-24 15:33:57 -0500

    Feature 3.3 Work, adding a line

--- DrivingLog/Views/TripDetailView.swift
+++ DrivingLog/Views/TripDetailView.swift
@@ -5,18 +5,38 @@
 //  Created by Alex Luke on 1/30/22.
 //
 import SwiftUI
+import MapKit
 
 struct TripDetailView: View {
     
     @ObservedObject var drivingLog: DrivingLog
     @State var trip: Trip
     let logsManager = DrivingLogsManager.sharedInstance
+    //Fake data/coordinates until 3.2 gets done
     
+    @State private var region = MKCoordinateRegion(
+        // Apple Park
+        center: CLLocationCoordinate2D(latitude: 37.334803, longitude: -122.008965),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+      )
+    @State private var lineCoordinates = [
+
+        // Steve Jobs theatre
+        CLLocationCoordinate2D(latitude: 37.330828, longitude: -122.007495),
+
+        // Caffè Macs
+        CLLocationCoordinate2D(latitude: 37.336083, longitude: -122.007356),
+
+        // Apple wellness center
+        CLLocationCoordinate2D(latitude: 37.336901, longitude:  -122.012345)
+      ];
     var body: some View {
         VStack {
-            Text("Map view here")
-                .font(.title3)
-                .padding()
+            LineMapView(
+                region: region,
+                lineCoordinates: lineCoordinates)
+              .ignoresSafeArea(edges: .top)
+              .frame(height: 300)
             
             Spacer()
             
