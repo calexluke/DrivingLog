bafd393ee1c24e5df8f82e3feabf30bf91e31b18
thqmos <90867589+thqmos@users.noreply.github.com>
2022-04-12 14:10:51 -0500

    Added Comments/Documentation

--- DrivingLog/Views/LineMapView.swift
+++ DrivingLog/Views/LineMapView.swift
@@ -27,24 +27,34 @@ struct LineMapView: UIViewRepresentable {
         return mapView
     }
     
+    /// This function updates the MKMapView to be put on the Trip Detail View.
+    /// - Parameter view: The MKMapView to be updated.
+    /// - Parameter context: The view's initial state.
     func updateUIView(_ view: MKMapView, context: Context) {
         let polyline = MKPolyline(coordinates: mapViewModel.routeCL, count: mapViewModel.routeCL.count)
         view.addOverlay(polyline)
     }
     
+    /// This function makes a Coordinator object.
+    /// - Returns: The coordinator object being made.
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
 }
 
+//Coordinator class
 class Coordinator: NSObject, MKMapViewDelegate {
     var parent: LineMapView
     
+    //Initialization Method
     init(_ parent: LineMapView) {
         self.parent = parent
     }
-    
+    /// This function returns an MKOverlayRenderer by taking a rendering in a polyline, a mapView, and an overlay. 
+    /// - Parameter mapView: The MKMapView to be worked with.
+    /// - Parameter overlay: The overlay to be rendered, as it will have the polyline.
+    /// - Returns: The visual representation of the MKOverlay.
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         if let routePolyline = overlay as? MKPolyline {
             let renderer = MKPolylineRenderer(polyline: routePolyline)
