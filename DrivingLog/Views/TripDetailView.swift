ef34e97ef03c839a1009c304dbd982a6a2775be0
thqmos <90867589+thqmos@users.noreply.github.com>
2022-04-12 13:59:20 -0500

    Added Comments/Documentation

--- DrivingLog/Views/TripDetailView.swift
+++ DrivingLog/Views/TripDetailView.swift
@@ -9,26 +9,31 @@ import MapKit
 
 struct TripDetailView: View {
     
+    //Initializing all of the variables and constants necessary for the Trip Detail View
     @ObservedObject var drivingLog: DrivingLog
     @ObservedObject var mapViewModel = MapViewModel()
     @State var trip: Trip
     let logsManager = DrivingLogsManager.sharedInstance
     
-
+    //Initializing a Driving Log object and a Trip object, both of which are needed for this view to function
     init(drivingLog: DrivingLog, selectedTrip: Trip) {
         self.drivingLog = drivingLog
         _trip = State<Trip>.init(initialValue: selectedTrip)
         setMapViewModel()
     }
     
+    //This is what the user will see when they load up the Trip Detail View
     var body: some View {
         VStack {
             
             Spacer()
+            
+            //Displaying the trip route
             tripRouteView()
             
             Spacer()
             
+            //Displaying supervisor unformation
             Text("Supervisor: \(trip.supervisorName)")
                 .foregroundColor(Theme.primaryTextColor)
                 .font(.title3)
@@ -36,6 +41,8 @@ struct TripDetailView: View {
             
             EditTripView(drivingLog: drivingLog, trip: trip)
         }
+        
+        //Working with UI design
         .background(
             Theme.appBackgroundColor
                 .ignoresSafeArea()
@@ -44,6 +51,8 @@ struct TripDetailView: View {
     }
     
     @ViewBuilder
+    /// This function returns a view depending on whether or not there is a route associated with a particular trip.
+    /// - Returns: A view either with or without the trip route, depending on if there is a route in the trip object.
     func tripRouteView() -> some View {
         if trip.route.isEmpty {
             Text("No route information available")
@@ -57,11 +66,13 @@ struct TripDetailView: View {
         }
     }
     
+    /// This function sets the Map View Model to have an appropriate location center, region, and polyline.
     func setMapViewModel() {
         guard let startingLocation = trip.route.first else {
             return
         }
         
+        //Centering the map on the appropriate location center, region, and polyline
         DispatchQueue.global(qos: .default).async {
             let startingLocationCL = CLLocationCoordinate2D(latitude: startingLocation.latitude,
                                                     longitude: startingLocation.longitude)
@@ -76,6 +87,8 @@ struct TripDetailView: View {
         }
     }
     
+    /// This function returns a MapViewModel which is created and set up by the function itself.
+    /// - Returns: A MapViewModel which has everything set up as necessary, such as the center, region, etc.
     func mapViewModelFromTrip() -> MapViewModel {
         guard let startingLocation = trip.route.first else {
             return MapViewModel()
@@ -89,6 +102,7 @@ struct TripDetailView: View {
     }
 }
 
+//This works to produce a view preview of a Trip Detail View in the Xcode IDE
 struct TripDetailView_Previews: PreviewProvider {
     static var previews: some View {
         TripDetailView(drivingLog: MockDrivingLog(), selectedTrip: MockDrivingLog().trips.first!)
